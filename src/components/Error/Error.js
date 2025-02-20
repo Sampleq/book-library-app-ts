b7b608eadc7f9678a51805247165a44122925ffe
Igor M. <samplefines@gmail.com>
2025-02-20 17:33:45 +0300

    put useEffect() in Error comp back - otherwise Error.js:17 Warning: Cannot update a component (`Lt`) while rendering a different component (`Error`)

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -11,21 +11,28 @@ function Error() {
 
   const { errorMessage } = useSelector(selectError);
 
-  //   useEffect(() => {
-  // в целом благодаря if - корректно работает и без  useEffect - useSelector сам ререндерит компонент при изменении возвращаемой его коллбеком части Состояния - seems no bug
-  if (errorMessage) {
-    toast.error(errorMessage, {
-      position: 'top-right',
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: false,
-      pauseOnHover: true,
-      draggable: false,
-    });
+  useEffect(() => {
+    // в целом благодаря if - корректно работает и без  useEffect - useSelector сам ререндерит компонент при изменении возвращаемой его коллбеком части Состояния - seems no bug - но нужен! - иначе ошибка Error.js:17 Warning: Cannot update a component (`Lt`) while rendering a different component (`Error`).
+    // Основная причина использования useEffect() — это предотвращение ошибки
+    // обновления состояния во время рендера компонента. useEffect нужен, чтобы
+    // избежать ошибки о недопустимом обновлении состояния во время
+    // рендера.Гарантирует, что dispatch(clearError()) вызывается только после
+    // рендера, предотвращая баги и предупреждения. Без useEffect код работает
+    // нестабильно и может вызвать непредсказуемые ошибки особенно в более
+    // сложных приложениях с большим количеством компонентов и состояний.
+    if (errorMessage) {
+      toast.error(errorMessage, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: false,
+      });
 
-    dispatch(clearError());
-  }
-  //   }, [errorMessage, dispatch]);
+      dispatch(clearError());
+    }
+  }, [errorMessage, dispatch]);
 
   return <ToastContainer />;
 }
